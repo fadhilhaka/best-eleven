8e2ca95f7f7a20d40a9aff224fc9a3039aa73e8a
fadhilhaka <fadhilhanri@gmail.com>
2018-05-26 18:28:49 +0700

    final project phase 0

--- README.md
+++
@@ -1,7 +0,0 @@
-# fadhilhaka.github.io
-
-About This Site: *Under Construction*
-
-Used Tags & Syntaxes: *Under Construction*
-
-About Me: Hai nama saya fadhil! salam kenal :)
---
+++ js/script.js
@@ -0,0 +1,59 @@
+function goalkeeper() {
+  $('.goalkeeper').fadeIn(1000);
+  $('.defender').fadeIn(1000);
+  $('.midfielder').fadeIn(1000);
+  $('.striker').fadeIn(1000);
+  $("#position-goalkeeper").click(function(){
+      $(this).data('clicked', true);
+    });
+  if($('#position-goalkeeper').data('clicked')) {
+    $('.defender').hide();
+    $('.midfielder').hide();
+    $('.striker').hide();
+  }
+}
+
+function defender() {
+  $('.goalkeeper').fadeIn(1000);
+  $('.defender').fadeIn(1000);
+  $('.midfielder').fadeIn(1000);
+  $('.striker').fadeIn(1000);
+  $("#position-defender").click(function(){
+      $(this).data('clicked', true);
+  });
+  if($('#position-defender').data('clicked')) {
+    $('.goalkeeper').hide();
+    $('.midfielder').hide();
+    $('.striker').hide();
+  }
+}
+
+function midfielder() {
+  $('.goalkeeper').fadeIn(1000);
+  $('.defender').fadeIn(1000);
+  $('.midfielder').fadeIn(1000);
+  $('.striker').fadeIn(1000);
+  $("#position-midfielder").click(function(){
+      $(this).data('clicked', true);
+  });
+  if($('#position-midfielder').data('clicked')) {
+    $('.goalkeeper').hide();
+    $('.defender').hide();
+    $('.striker').hide();
+  }
+}
+
+function striker() {
+  $('.goalkeeper').fadeIn(1000);
+  $('.defender').fadeIn(1000);
+  $('.midfielder').fadeIn(1000);
+  $('.striker').fadeIn(1000);
+  $("#position-striker").click(function(){
+      $(this).data('clicked', true);
+  });
+  if($('#position-striker').data('clicked')) {
+    $('.goalkeeper').hide();
+    $('.defender').hide();
+    $('.midfielder').hide();
+  }
+}
